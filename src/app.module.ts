380245368d951b65ffed27b9657d95057efef09f
김영훈 <hoony@gim-yeonghun-ui-MacBookAir.local>
2021-02-08 15:10:59 +0900

    Scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+import { ApplicationModule } from './application/application.module';
+import { CertificateModule } from './certificate/certificate.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    AuthModule,
+    PostModule,
+    ApplicationModule,
+    CertificateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
