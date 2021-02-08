49965ccaba78aed8ad9c5c64851a2ed6e904e16a
김영훈 <hoony@gim-yeonghun-ui-MacBookAir.local>
2021-02-08 15:33:48 +0900

    feat🌟:Import graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
 import { ApplicationModule } from './application/application.module';
 import { CertificateModule } from './certificate/certificate.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
     UserModule,
     AuthModule,
     PostModule,
