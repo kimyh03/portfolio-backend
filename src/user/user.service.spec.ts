b8c26fc438e31dbd7445b1e11b7d026ca31d29d2
김영훈 <kimyh03@gmail.com>
2021-02-15 14:59:34 +0900

    test 🔍 : Set userService test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,63 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AuthService } from 'src/auth/auth.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  findOneOrFail: jest.fn(),
+  delete: jest.fn(),
+});
+
+const mockAuthService = () => ({
+  signToken: jest.fn(),
+});
+
+const mockConfigService = () => ({
+  get: jest.fn(),
+});
+
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: MockRepository<User>;
 
+  let authService: AuthService;
+  let configService: ConfigService;
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository(),
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService(),
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService(),
+        },
+      ],
     }).compile();
-
-    service = module.get<UserService>(UserService);
+    userRepository = module.get(getRepositoryToken(User));
+    userService = module.get<UserService>(UserService);
+    authService = module.get<AuthService>(AuthService);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(authService).toBeDefined();
+    expect(configService).toBeDefined();
   });
 });
