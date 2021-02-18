79ecfd5d0756078f2699b437c044552d62d61656
김영훈 <kimyh03@gmail.com>
2021-02-18 11:33:50 +0900

    test 🔍 : Set postService test

--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -1,18 +1,38 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post, postRigionEnum } from './entities/post.entity';
 import { PostService } from './post.service';
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  findOneOrFail: jest.fn(),
+  delete: jest.fn(),
+  find: jest.fn(),
+});
 
 describe('PostService', () => {
-  let service: PostService;
+  let postService: PostService;
+  let postRepository: MockRepository<Post>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+    const module = await Test.createTestingModule({
+      providers: [
+        PostService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
-
-    service = module.get<PostService>(PostService);
+    postService = module.get<PostService>(PostService);
+    postRepository = module.get(getRepositoryToken(Post));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(postService).toBeDefined();
+    expect(postRepository).toBeDefined();
   });
 });
