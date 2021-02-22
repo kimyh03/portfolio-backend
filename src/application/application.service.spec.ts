87d45ff252d8e9bd2145a26dbdd1ad1b2f65a081
김영훈 <kimyh03@gmail.com>
2021-02-22 19:47:57 +0900

    test 🔍 : Add toggleApply test todo

--- src/application/application.service.spec.ts
+++ src/application/application.service.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ApplicationService } from './application.service';
+import { Application } from './entities/application.entity';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  findOneOrFail: jest.fn(),
+  delete: jest.fn(),
+  find: jest.fn(),
+  remove: jest.fn(),
+});
 
 describe('ApplicationService', () => {
   let service: ApplicationService;
+  let applicationRepository: MockRepository<Application>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApplicationService],
+      providers: [
+        ApplicationService,
+        {
+          provide: getRepositoryToken(Application),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<ApplicationService>(ApplicationService);
+    applicationRepository = module.get(getRepositoryToken(Application));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('toggleApply', () => {
+    it.todo('should fail with not found post id');
+    it.todo('should fail if isOpen false');
+    it.todo('should create application(apply for post)');
+    it.todo('should delete application(cancel application)');
   });
 });
