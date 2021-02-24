08913fd8510e9c39b90309a257c2fd945da0e832
김영훈 <kimyh03@gmail.com>
2021-02-24 14:23:39 +0900

    test 📝 : Set e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,17 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
   });
 
   it('/ (GET)', () => {
