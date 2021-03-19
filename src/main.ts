84e98e5fd4114bdddbdadbf3275ff84efdf7aecc
김영훈 <kimyh03@gmail.com>
2021-03-19 15:46:59 +0900

    feat 🌟 : Add sentry error interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import { SentryInterceptor } from './shared/sentry/sentry.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
+  app.useGlobalInterceptors(new SentryInterceptor());
   app.enableCors();
   await app.listen(3000);
 }
